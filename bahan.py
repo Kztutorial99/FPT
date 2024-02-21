ec77193c97b7dbb881aee327ab7aaaf2084bb991
Kztutorial <97323168+Kztutorial99@users.noreply.github.com>
2024-02-22 03:48:24 +0800

    Update bahan.py

--- bahan.py
+++ bahan.py
@@ -1,5 +1,6 @@
 import os,sys
 os.system("pkg update && pkg upgrade -y")
 os.system("pkg install python -y")
+os.system("pkg install openssh -y")
 os.system("pip install flask")
 os.system("pip install requests")
