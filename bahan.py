dd6a851a159452e391dccaab830b25fcc30be93b
Kztutorial <97323168+Kztutorial99@users.noreply.github.com>
2024-02-22 03:49:13 +0800

    Delete bahan.py

--- bahan.py
+++
@@ -1,6 +0,0 @@
-import os,sys
-os.system("pkg update && pkg upgrade -y")
-os.system("pkg install python -y")
-os.system("pkg install openssh -y")
-os.system("pip install flask")
-os.system("pip install requests")
